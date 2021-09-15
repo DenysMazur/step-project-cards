0f39021da7cf761af3455a0caa2f2c227be05ba4
Denis Mazur <dmazur.ua@gmail.com>
2021-09-15 12:07:23 +0300

    add alert class to input and change validation

--- scripts/input.js
+++ scripts/input.js
@@ -1,17 +1,28 @@
 import Element from './element.js';
 
 export default class Input extends Element {
-  constructor(type, classes = [], placeholder) {
+  constructor(type, classes = [], placeholder, required = 'true') {
     super();
     this.type = type;
     this.placeholder = placeholder;
-    this.classes = classes
+    this.classes = classes;
+    this.required = required;
   }
 
   createInput() {
     const input = super.createElement('input', this.classes);
     input.setAttribute('type', this.type);
     input.setAttribute('placeholder', this.placeholder);
+    input.setAttribute('required', true);
+    this.checkAlertClass(input);
     return input;
+  }
+
+  checkAlertClass(input) {
+    input.addEventListener('change', () => {
+      if (input.value !== '') {
+        input.classList.remove('alert', 'alert-danger');
+      }
+    })
   }
 }
